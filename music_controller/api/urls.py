8b1eb4506a03a353a0ac5d8adbf833aa55d4385c
Roger <rogernuica3@gmail.com>
2021-01-11 21:49:35 +0200

    room update and better organisation

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,7 +2,7 @@
 high level support for doing this and that.
 """
 from django.urls import path
-from .views import RoomView,CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from .views import RoomView,CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave-room', LeaveRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view())
 ]
